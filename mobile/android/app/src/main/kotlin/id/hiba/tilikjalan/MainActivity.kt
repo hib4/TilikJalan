c67cfa9f774d0e8544ef8c4af0e2c7ee787fa515
Hibatullah Fawwaz Hana <hiba.kudus@gmail.com>
2025-08-02 00:24:01 +0700

    Init mobile app

---
+++ mobile/android/app/src/main/kotlin/id/hiba/tilikjalan/MainActivity.kt
@@ -0,0 +1,5 @@
+package id.hiba.tilikjalan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ mobile/lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:tilikjalan/app/app.dart';
+import 'package:tilikjalan/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
